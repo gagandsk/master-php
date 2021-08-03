469e390ef48a84629a4efcd775bbd36f750586d4
ItsGagan20 <gagandeps0gmail.com>
2021-08-03 16:52:07 +0200

    arithmetic operators

---
+++ aprendiendo-php/06-operadores/index.php
@@ -0,0 +1,11 @@
+<?php
+//Operadores Aritméticos
+$numero1 = 550;
+$numero2 = 3;
+
+echo 'Suma: '.($numero1+$numero2).'<br/>';
+echo 'Resta: '.($numero1-$numero2).'<br/>';
+echo 'Division: '.($numero1/$numero2).'<br/>';
+echo 'Multiplicacion: '.($numero1*$numero2).'<br/>';
+echo 'Resto: '.($numero1%$numero2).'<br/>'; //Modulo o Resto de una division
+?>
