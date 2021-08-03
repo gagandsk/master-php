7aa219e7da838ec1a5c49c3a51bc1446f8ac8700
ItsGagan20 <gagandeps0gmail.com>
2021-08-03 17:13:27 +0200

    server variables

---
+++ aprendiendo-php/07-variables-superglobales/index.php
@@ -0,0 +1,25 @@
+<?php
+//VARIABLES SUPERGLOBALES
+
+//Variables de servidor
+echo '<h1>';
+echo $_SERVER['SERVER_ADDR']; //IP DEL SERVIDOR
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['SERVER_NAME'];
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['SERVER_SOFTWARE'];
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['HTTP_USER_AGENT'];
+echo '</h1>';
+
+echo '<h1>';
+echo $_SERVER['REMOTE_ADDR']; //IP DEL CLIENTE
+echo '</h1>';
+
+?>
