efadf6a5c00a95933dcffc09cde3ebfc49d38d7c
ItsGagan20 <gagandeps0gmail.com>
2021-08-03 14:33:14 +0200

    variables in PHP

---
+++ aprendiendo-php/03-variables/index.php
@@ -0,0 +1,19 @@
+<?php
+
+//variables
+
+$mi_primera_variable = "Hola mundo desde una variable";
+$numero = 10;
+$verdadero = true; //1
+
+//la variable se sustituye
+$numero = 50;
+
+echo '<h1>'.$mi_primera_variable.'</h1>';
+echo $numero .'<br/>';
+echo $verdadero.'<br/>';
+
+$numero = 90;
+echo $numero .'<br/>';
+
+?>
