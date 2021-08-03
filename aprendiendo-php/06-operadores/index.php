ac535cd611b8bf5bc5791ad3bad65451b845605d
ItsGagan20 <gagandeps0gmail.com>
2021-08-03 16:57:22 +0200

    increment and decrement operators

--- aprendiendo-php/06-operadores/index.php
+++ aprendiendo-php/06-operadores/index.php
@@ -8,4 +8,23 @@ echo 'Resta: '.($numero1-$numero2).'<br/>';
 echo 'Division: '.($numero1/$numero2).'<br/>';
 echo 'Multiplicacion: '.($numero1*$numero2).'<br/>';
 echo 'Resto: '.($numero1%$numero2).'<br/>'; //Modulo o Resto de una division
+
+
+//Operadores incremento y decremento
+$year = 2019;
+
+//Incremento
+$year++; //$year = $year + 1;
+
+//Decremento
+$year--; //$year = $year - 1;
+
+//Pre-incremento
+++$year; //$year = 1 + $year;
+
+//Pre-decremento
+--$year; //$year = 1 - $year;
+
+echo '<h1>'.$year.'<h1/>'
+
 ?>
