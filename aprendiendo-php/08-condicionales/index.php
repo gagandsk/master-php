58bde0121df47c7f46bf24684902ec5d8d69822e
ItsGagan20 <gagandeps0gmail.com>
2021-08-04 11:59:20 +0200

    comparison operators in php

--- aprendiendo-php/08-condicionales/index.php
+++ aprendiendo-php/08-condicionales/index.php
@@ -22,7 +22,7 @@
 >= (MAYOR O IGUAL QUE)
 
 */
-
+//Ejemplo 1
 $color = "Verde";
 if($color=="Rojo"){
     echo "El color ES rojo";
@@ -32,11 +32,29 @@ if($color=="Rojo"){
 
 echo "<br/>";
 
+//Ejemplo 2
 $year = 2029;
 if($year >= 2019){
     echo "Hemos pasado el 2019";
 }else{
     echo "Es un año anterioir a 2019";
+}
+
+//Ejemplo 3
+$nombre = "Roronoa Zoro";
+$edad = 21;
+$mayor_edad = 18;
+$ciudad = "Wano";
+$continente = "Asia";
+if($edad >= $mayor_edad){
+    echo "<h1>$nombre es major de edad</h1>";
+    if($continente == "Asia"){
+        echo "<h2>Y es de $ciudad</h2>";
+    }else{
+        echo "No es asiatico";
+    }
+}else{ 
+    echo "<h2>$nombre no es mayor de edad</h2>";
 }
 
 ?>
