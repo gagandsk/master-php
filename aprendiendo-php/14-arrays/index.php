5512141029e75b2ce5456da78addf52170358d50
ItsGagan20 <gagandeps0gmail.com>
2021-08-05 19:45:06 +0200

    iterate array with loop

---
+++ aprendiendo-php/14-arrays/index.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * ARRAY: un array es una coleccion o conjunto de datos/valores, bajo un unico nombre. 
+ * Para acceder a esos valores podemos utilizar un indice numerico o alfanumerico
+ * 
+*/
+
+$animes = array('One Piece', 'Dragon ball Z', 'Attack on titans');
+$juegos = ['The Witcher 3', 'Red Dead Redemprion 2', 'GTA V'];
+//var_dump($animes[2]);
+//var_dump($juegos);
+
+/*echo $animes[0]."<br/>";
+echo $juegos[1]."<br/>";*/
+
+//Recorrer array con FOR
+echo "<h1>Listado de animes</h1>";
+
+echo "<ul>";
+for($i = 0; $i< count($animes); $i++){
+    echo "<li>".$animes[$i]."</li>";
+}
+echo "</ul>";
+
+
+//Recorrer array con Foreach
+echo "<ul>";
+foreach($animes as $anime){
+    echo "<li>".$anime."</li>";
+}
+echo "</ul>";
+?>
