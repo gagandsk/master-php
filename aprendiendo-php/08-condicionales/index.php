8b1e231cfca432c470c1a10eb8f2c17e80bb908a
ItsGagan20 <gagandeps0gmail.com>
2021-08-05 11:13:14 +0200

    control structure 'switch'

--- aprendiendo-php/08-condicionales/index.php
+++ aprendiendo-php/08-condicionales/index.php
@@ -105,4 +105,28 @@ if($pais == "Mexico" || $pais == "Espanña" || $pais == "Colombia"){
     echo "En este pais no se habla español";
 }
 
+echo "<hr>";
+
+//SWITCH
+$dia = 2;
+switch ($dia){
+    case 1:
+        echo "LUNES";
+        break;
+    case 2:    
+        echo "MARTES";
+        break;
+    case 3:    
+        echo "MIERCOLES";
+        break;    
+    case 4:    
+        echo "JUEVES";
+        break;
+    case 5:    
+        echo "VIERNES";
+        break;
+    default:
+        echo "FIN DE SEMANA";    
+}
+
 ?>
