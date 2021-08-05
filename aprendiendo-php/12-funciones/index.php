30fa26aad1d795aa9f8c23d9d2d322d10d7a6a25
ItsGagan20 <gagandeps0gmail.com>
2021-08-05 13:34:12 +0200

    functions

---
+++ aprendiendo-php/12-funciones/index.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+FUNCIONES: Conjunto de instrucciones agrupadas bajo un nombre concreto y que 
+pueden utilizarse solamente invocando a la funcion tantas veces como queramos
+function nombreDeMiFuncion($parametro){
+    //bloque de instrucciones
+}
+
+nombreDeMiFuncion($mi_parametro);
+*/
+
+//Ejemplo 1
+function holaMundo(){
+    echo "<h1>1</h1>";
+    echo "<h1>2</h1>";
+    echo "<h1>3</h1>";
+    echo "<hr/>";
+}
+//Invocar funcion
+holaMundo();
+holaMundo();
+holaMundo();
+
+
+//Ejemplo 2
+
+function tabla ($numero){
+    echo "<h3>Tabla de multiplicar del numero: $numero</h3>";
+    for($i=1;$i<=10;$i++){
+        $operacion = $numero*$i;
+        echo "$numero x $i = ".$operacion."<br/>";
+    }
+}
+
+tabla(5);
+
+echo "<hr/>";
+
+if(isset($_GET['numero'])){
+    tabla($_GET["numero"]);
+    tabla(2);
+}else{
+    echo "No hay numero para hacer la multiplicacion";
+}
+
+echo "<hr/>";
+echo "<hr/>";
+echo "Todas las tablas de multiplicar del 1-10";
+for($i=1;$i<=10;$i++){
+    tabla($i);
+}
+
+?>
