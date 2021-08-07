bdafaec0e97c9eef265345ee6fcaa1c450cb9c51
gagan-daka <gagandeps0@gmail.com>
2021-08-08 00:32:57 +0200

    validate form (html5) ,get error and show it,used constants for validate data,regular expressions

--- aprendiendo-php/19-validacion/index.php
+++ aprendiendo-php/19-validacion/index.php
@@ -10,12 +10,46 @@
 
 <body>
     <h1>Validar formularios en PHP</h1>
+
+
+    <?php if (isset($_GET['error'])) {
+
+        $error = $_GET['error'];
+
+        if ($error == 'faltan_valores') {
+            echo '<strong style="color:red">Introduce bien todos los valores</strong>';
+        }
+
+        if ($error == 'error_nombre') {
+            echo '<strong style="color:red">Introduce bien el nombre</strong>';
+        }
+
+        if ($error == 'error_apellido') {
+            echo '<strong style="color:red">Introduce bien el apellido</strong>';
+        }
+
+        if ($error == 'error_edad') {
+            echo '<strong style="color:red">Introduce bien la edad</strong>';
+        }
+
+        if ($error == 'error_email') {
+            echo '<strong style="color:red">Introduce bien el email</strong>';
+        }
+
+        if ($error == 'error_password') {
+            echo '<strong style="color:red">Introduce una contrasenya que tengas mas de 5 caracteres</strong>';
+        }
+    } ?>
+
+
     <form method="POST" action="procesar_formulario.php">
+
+        <!--VALIDAR CON HTML Y DEL LADO DEL SERVIDOR A LA VEZ :D-->
         <label for="nombre">Nombre</label><br />
         <input type="text" name="nombre" required="required" pattern="[A-Za-z]+"><br />
 
         <label for="apellido">Apellido</label><br />
-        <input type="text" name="apellido" pattern="[A-Za-z]+"><br />
+        <input type="text" name="apellidos" pattern="[A-Za-z]+"><br />
 
         <label for="edad">Edad</label><br />
         <input type="number" name="edad" pattern="[0-9]+"><br />
@@ -28,6 +62,26 @@
 
         <input type="submit" name="Enviar">
 
+
+        <!--VALIDAR DESDE EL LADO DEL SERVIDOR   -->
+    <!--
+        <label for="nombre">Nombre</label><br />
+        <input type="text" name="nombre"><br />
+
+        <label for="apellido">Apellido</label><br />
+        <input type="text" name="apellidos"><br />
+
+        <label for="edad">Edad</label><br />
+        <input type="number" name="edad"><br />
+
+        <label for="email">Email</label><br />
+        <input type="text" name="email"><br />
+
+        <label for="pass">Contraseña</label><br />
+        <input type="password" name="pass"><br />
+
+        <input type="submit" name="Enviar">
+    -->
     </form>
 </body>
 
--- aprendiendo-php/19-validacion/procesar_formulario.php
+++ aprendiendo-php/19-validacion/procesar_formulario.php
@@ -1,2 +1,78 @@
 <?php
+
+$error = 'faltan_valores';
+
+if (!empty($_POST['nombre']) && !empty($_POST['apellidos']) && !empty($_POST['edad']) && !empty($_POST['email']) && !empty($_POST['pass'])){
+
+    
+    $error = 'ok';
+
+    $nombre = $_POST['nombre'];
+    $apellidos = $_POST['apellidos'];
+    $edad = (int) $_POST['edad'];
+    $email = $_POST['email'];
+    $password = $_POST['pass'];
+
+    //Validar el nombre
+    if (!is_string($nombre) || preg_match("/[0-9]/", $nombre)) {
+        $error = 'error_nombre';
+    }
+
+    //Validar el apellidos
+    if (!is_string($apellidos) ||  preg_match("/[0-9]/", $apellidos)) {
+        $error = 'error_apellido';
+    }
+
+    //Validar la edad
+    if (!is_integer($edad) || !filter_var($edad, FILTER_VALIDATE_INT)) {
+        $error = 'error_edad';
+    }
+
+    //Validar email
+    if (!is_string($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $error = 'error_email';
+    }
+
+    //Validar la password
+    
+    if (empty($password) || strlen($password) < 5) {
+        $error = 'error_password';
+    }
+
+    /*
+    var_dump($_POST);
+    var_dump($error);
+    die();
+    */
+    
+} else {
+    $error = 'faltan_valores';
+}
+
+if($error != 'ok'){
+    header("Location:index.php?error=$error");
+}
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Validacion de formularios PHP</title>
+</head>
+
+<body>
+    <?php if ($error == 'ok') : ?>
+        <h1>Datos validados Correctamente</h1>
+        <p><?= $nombre ?></p>
+        <p><?= $apellidos ?></p>
+        <p><?= $edad ?></p>
+        <p><?= $email ?></p>
+    <?php endif; ?>
+</body>
+
+</html>
