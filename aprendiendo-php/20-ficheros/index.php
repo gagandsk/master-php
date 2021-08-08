0422216b943aa5bcb59d5e8f91fcc30d2c279087
gagan-daka <gagandeps0@gmail.com>
2021-08-08 20:56:45 +0200

    manipulate file

--- aprendiendo-php/20-ficheros/index.php
+++ aprendiendo-php/20-ficheros/index.php
@@ -14,10 +14,20 @@ while(!feof($archivo )){
 }
 
 //Escribir 
-$texto = "Texto insertado desde PHP";
-fwrite($archivo, $texto);
+/*$texto = "Texto insertado desde PHP";
+fwrite($archivo, $texto);*/
 
 //Cerrar archivo
 fclose($archivo);
+
+
+//Copiar archivo
+//copy($file_name, 'fichero_copiado.txt') or die("Error al copiar");
+
+//Renombrar Fichero
+//rename('fichero_copiado.txt', 'fichero_copiado_renombrado.txt');
+
+//Eliminar fitchero
+//unlink('fichero_copiado_renombrado.txt') or die("Error al borrar");
 
 ?>
