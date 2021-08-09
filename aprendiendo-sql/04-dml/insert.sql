564026cef98ff0856c1c01d0f23a319afe30803d
gagan-daka <gagandeps0@gmail.com>
2021-08-09 14:08:41 +0200

    'INSERT INTO' statement

---
+++ aprendiendo-sql/04-dml/insert.sql
@@ -0,0 +1,12 @@
+/*INSERTAR NUEVOS REGISTROS*/
+INSERT INTO usuarios VALUES(null,'Sanji','Sanji','sanji@gmail.com','123','2021-08-05');
+INSERT INTO usuarios VALUES(null,'Nami','Nami','Nami@gmail.com','741','2021-08-05');
+INSERT INTO usuarios VALUES(null,'Nico Robin','Nico Robin','nicoRobin@gmail.com','852','2021-08-05');
+INSERT INTO usuarios VALUES(null,'Ussop','Ussop','ussop@gmail.com','963','2021-08-05');
+INSERT INTO usuarios VALUES(null,'Zoro','Zoro','zoro@gmail.com','951','2021-08-05');
+INSERT INTO usuarios VALUES(null,'Chooper','Tony','tony@gmail.com','425','2021-08-05');
+INSERT INTO usuarios VALUES(null,'Luffy','Monkey D','barretPalla@gmail.com','625','2021-08-05');
+
+
+/*INSERTAR FILAS SOLO SON CIERTAS COLUMNAS*/
+INSERT INTO usuarios (email, password) values ('email@gmail.com','AAaa');
