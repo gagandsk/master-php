8a4cec2da0764cb3a13247e4993d942104bf4ca0
gagan-daka <gagandeps0@gmail.com>
2021-08-09 13:14:25 +0200

    column modifiers

--- aprendiendo-sql/02-modificar-tablas/modificar.sql
+++ aprendiendo-sql/02-modificar-tablas/modificar.sql
@@ -3,9 +3,20 @@
 ALTER TABLE usuarios RENAME TO usuarios_renom;
 
 -- CANVIAR NOMBRE DE UNA COLUMNA
-/*ALTER TABLE nombre_table CHANGE campo_a_canviar nuevo_nombre_del_campo_a_canviar tipo_dato() tipo_restriccion;*/
+/*ALTER TABLE nombre_table CHANGE campo_a_canviar nuevo_nombre_del_campo_a_canviar tipo_dato tipo_restriccion;*/
 ALTER TABLE usuarios CHANGE apellidos apellido varchar(100) null;
 
 -- MODIFICAR COLUMNA SIN CANVIAR EL NOMBRE 
-/*ALTER TABLE nombre_table MODIFY nombre_campo tipo_dato() tipo_restriccion;*/
+/*ALTER TABLE nombre_table MODIFY nombre_campo tipo_dato tipo_restriccion;*/
 ALTER TABLE usuarios MODIFY apellido char(50) not null;
+
+-- AÑADIR NUEVA COLUMNA
+/*ALTER TABLE nombre_table ADD nombre_nueva_columna tipo_dato tipo_restriccion*/
+ALTER TABLE usuarios ADD bandera varchar(50) null;
+
+-- AÑADIR RESTRICCION A COLUMNA
+ALTER TABLE usuarios ADD CONSTRAINT uq_email UNIQUE (email);
+
+-- BORRAR UNA COLUMNA
+/*ALTER TABLE nombre_table DROP nombre_columna;*/
+ALTER TABLE usuarios DROP bandera;
