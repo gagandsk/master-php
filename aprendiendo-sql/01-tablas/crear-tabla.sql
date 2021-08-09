f979dcb76f80be828b298c3b3347fd1ae4773760
gagan-daka <gagandeps0@gmail.com>
2021-08-09 12:24:55 +0200

    introduction to sql

---
+++ aprendiendo-sql/01-tablas/crear-tabla.sql
@@ -0,0 +1,29 @@
+/*
+TIPOS DE DATOS  
+    - int(numero de cifras)      ENTERO
+    - integer(numero de cifras)  ENTERO (maximo 4294967295)
+    - varchar(numero de cifras)  STRING / VALOR ALFANUMERICO  (max. 255 caracteres)
+    - char (numero de cifras)    STRING / VALOR ALFANUMERICO
+    - float(numero de decimales) DECIMAL / COMA FLOTANTE 
+    - date, time, timestamp
+
+    //STRING MAS GRANDES
+    - TEXT (max. 65535 caracteres)
+    - MEDIUMTEXT (max. 16 millones de caracteres)
+    - LONGTEXT (max. 4 billones de caracteres)
+
+    //ENTEROS MAS GRANDES
+    - MEDIUMINT
+    - BIGINT
+*/
+/*desc nombr_table (muestra como esta formada esa tabla)*/
+
+/*CREAR TABLA (el nombre de las tablas siempre en plural)*/
+
+CREATE TABLE usuarios (
+    id         int(11),
+    nombre     varchar(100),
+    apellidos  varchar(255),
+    email      varchar(100),
+    password   varchar(255)
+);
