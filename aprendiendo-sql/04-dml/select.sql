10cea76d4f5e451fd4cd458e45ced3692afd71b7
gagan-daka <gagandeps0@gmail.com>
2021-08-10 13:14:34 +0200

    math functions

--- aprendiendo-sql/04-dml/select.sql
+++ aprendiendo-sql/04-dml/select.sql
@@ -6,3 +6,13 @@ SELECT * FROM usuarios;
 
 -- OPERADORES ARITMETICOS
 SELECT email , (4+7) AS 'Operacion' FROM usuarios;
+
+-- FUNCIONES MATEMATICAS
+SELECT ABS(7) AS 'Operacion' FROM usuarios;
+SELECT CEIL(7.2) AS 'Operacion' FROM usuarios;
+SELECT FLOOR(7.2) AS 'Operacion' FROM usuarios;
+SELECT PI() AS 'Operacion' FROM usuarios;
+SELECT RAND() AS 'Operacion' FROM usuarios;
+SELECT ROUND(id, 2) AS 'Operacion' FROM usuarios;
+SELECT SQRT(7.82) AS 'Operacion' FROM usuarios;
+SELECT TRUNCATE(7.82, 2) AS 'Operacion' FROM usuarios; -- QUITAR DECIMALES
