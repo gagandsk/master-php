949e836336d626de47927072d68ab850005a7d26
gagan-daka <gagandeps0@gmail.com>
2021-08-10 13:56:41 +0200

    clause 'where' and wildcards

--- aprendiendo-sql/04-dml/where.sql
+++ aprendiendo-sql/04-dml/where.sql
@@ -3,12 +3,12 @@ SELECT * FROM usuarios WHERE email = 'sanji@gmail.com';
 
 /*
 OPERADORES DE COMPARACIÓN
-    * =  (igual)
-    * != (distinto)
-    * <  (menor)
-    * >  (mayor)
-    * <= (menor o igual)
-    * >= (mayor o igual)
+    * =        (igual)
+    * !=       (distinto)
+    * <        (menor)
+    * >        (mayor)
+    * <=       (menor o igual)
+    * >=       (mayor o igual)
     * BETWEEN
     * IN
     * IS NULL
@@ -17,8 +17,25 @@ OPERADORES DE COMPARACIÓN
     * NOT LIKE
 */
 
+
+/*
+OPERADORES LOGICOS
+    * OR
+    * AND
+    * NOT
+*/
+
+/*
+COMODINES
+    * CUALQUIER CANTIDAD DE CARACTERES : %
+    * UN CARACTER DESCONOCIDO: _
+*/
+
 -- EJEMPLOS
 /*1. Mostrar nombre y apellidos de todos los usuarios registrados en el 2021*/
 SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha) = 2021; 
-/*1. Mostrar nombre y apellidos de todos los usuarios registrados QUE NO se registraron en 2021*/
+/*2. Mostrar nombre y apellidos de todos los usuarios registrados QUE NO se registraron en 2021*/
 SELECT nombre, apellidos FROM usuarios WHERE YEAR(fecha) != 2021 OR ISNULL(fecha); 
+
+/*3. Mostrar el email de los usuarios cuyo apellido contenga la letra A y su contraseña sea 123*/
+SELECT email FROM usuarios WHERE apellidos LIKE '%a%' AND password = '123';
