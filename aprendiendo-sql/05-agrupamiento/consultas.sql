20340adf40c86dd7a043a171c35c6092ad932b69
gagan-daka <gagandeps0@gmail.com>
2021-08-11 13:37:51 +0200

    'AVG' function

--- aprendiendo-sql/05-agrupamiento/consultas.sql
+++ aprendiendo-sql/05-agrupamiento/consultas.sql
@@ -8,3 +8,13 @@ SELECT COUNT(titulo) AS 'Numero de entradas', categoria_id FROM entradas GROUP B
 /*HACING es lo mismo que 'WHERE', pero sirve para consultas de agrupamiento*/
 SELECT COUNT(titulo) AS 'Numero de entradas', categoria_id 
 FROM entradas GROUP BY categoria_id HAVING COUNT(titulo) >= 1;
+
+/*
+AVG     Sacar la media
+COUNT   Contar numero de elementos
+MAX     Devuelve el valor maximo del grupo
+MIN     Devuelve el valor minimo del grupo
+SUM     Sumar todo el contenido del grupo
+*/
+
+SELECT AVG(id) AS 'Media de entradas' FROM entradas;
