dea0edee061fc36cb1c5104b6332c343238313f5
gagan-daka <gagandeps0@gmail.com>
2021-08-11 12:23:07 +0200

    inserts for tables  'categoria' and 'entradas'

---
+++ aprendiendo-sql/05-agrupamiento/insert.sql
@@ -0,0 +1,28 @@
+-- INSERTS  PARA CATEGORIAS
+INSERT INTO categorias VALUES(null, 'Capitan');
+INSERT INTO categorias VALUES(null, 'Medico');
+INSERT INTO categorias VALUES(null, 'Cocinero');
+INSERT INTO categorias VALUES(null, 'Tripulante');
+INSERT INTO categorias VALUES(null, 'Combatiente');
+INSERT INTO categorias VALUES(null, 'Francotirador');
+INSERT INTO categorias VALUES(null, 'Arqueóloga');
+INSERT INTO categorias VALUES(null, 'Carpintero');
+INSERT INTO categorias VALUES(null, 'Músico');
+INSERT INTO categorias VALUES(null, 'Navegante');
+
+
+
+-- INSERTS  PARA ENTRADAS
+    -- Usuario 1
+INSERT INTO entradas VALUES(null, 1, 32, 'Monkey D. Luffy', 'Llegar a ser el Rey de los Piratas', CURDATE());
+INSERT INTO entradas VALUES(null, 1, 33, 'Tony Tony Chopper', 'Ser capaz de curar cualquier enfermedad', CURDATE());
+INSERT INTO entradas VALUES(null, 1, 34, 'Sanji', 'Encontrar el "All Blue"', CURDATE());
+    -- Usuario 2
+INSERT INTO entradas VALUES(null, 2, 36, 'Roronoa Zoro', 'Llegar a ser el mejor espadachín del mundo', CURDATE());
+INSERT INTO entradas VALUES(null, 2, 41, 'Nami', 'Dibujar un mapa de todo el mundo', CURDATE());
+INSERT INTO entradas VALUES(null, 2, 37, 'Usopp', 'Convertirse en un valiente pirata y visitar Elbaf, la tierra de los valientes guerreros', CURDATE());
+    -- Usuario 3
+INSERT INTO entradas VALUES(null, 3, 38, 'Nico Robin', 'Encontrar el Rio Poneglyph y descubrir qué ocurrió en los cien años de historia perdida', CURDATE());
+INSERT INTO entradas VALUES(null, 3, 39, 'Franky', 'Hacer un barco de ensueño y darle la vuelta al mundo como su carpintero de a bordo.', CURDATE());
+INSERT INTO entradas VALUES(null, 3, 40, 'Brook', '	Reencontrarse con Laboon', CURDATE());
+
