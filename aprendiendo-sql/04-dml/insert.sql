f6f1fa4aa0db031a201cd04085d5b2acb510bb11
gagan-daka <gagandeps0@gmail.com>
2021-08-11 12:04:51 +0200

    'user table' information updated

--- aprendiendo-sql/04-dml/insert.sql
+++ aprendiendo-sql/04-dml/insert.sql
@@ -1,13 +1,10 @@
 /*INSERTAR NUEVOS REGISTROS*/
-INSERT INTO usuarios VALUES(null,'Sanji','Sanji','sanji@gmail.com','123','2021-08-05');
-INSERT INTO usuarios VALUES(null,'Nami','Nami','Nami@gmail.com','741','2021-08-05');
-INSERT INTO usuarios VALUES(null,'Nico Robin','Nico Robin','nicoRobin@gmail.com','852','2021-08-05');
-INSERT INTO usuarios VALUES(null,'Ussop','Ussop','ussop@gmail.com','963','2021-08-05');
-INSERT INTO usuarios VALUES(null,'Zoro','Zoro','zoro@gmail.com','951','2021-08-05');
-INSERT INTO usuarios VALUES(null,'Chooper','Tony','tony@gmail.com','425','2021-08-05');
-INSERT INTO usuarios VALUES(null,'Luffy','Monkey D','barretPalla@gmail.com','625','2021-08-05');
-INSERT INTO usuarios VALUES(null,'admin','admin','admin@gmail.com','admin','2020-08-05');
+INSERT INTO usuarios VALUES(null,'Gagan','gagan','gagan@gmail.com','123','2021-08-05');
+INSERT INTO usuarios VALUES(null,'Jotta','jotta','jotta@gmail.com','741','2021-08-05');
+INSERT INTO usuarios VALUES(null,'Victor Robin','robles','robles5@gmail.com','852','2021-08-05');
+
 
 
 /*INSERTAR FILAS SOLO SON CIERTAS COLUMNAS*/
 INSERT INTO usuarios (email, password) values ('email@gmail.com','AAaa');
+
