aff5a6264a34853c46de0b5299e7cb2d3a18ac50
gagan-daka <gagandeps0@gmail.com>
2021-08-11 13:40:18 +0200

    'SUM' function

--- aprendiendo-sql/05-agrupamiento/consultas.sql
+++ aprendiendo-sql/05-agrupamiento/consultas.sql
@@ -20,3 +20,4 @@ SUM     Sumar todo el contenido del grupo
 SELECT AVG(id) AS 'Media de entradas' FROM entradas;
 SELECT MAX(id) AS 'MAXIMO ID' , titulo FROM entradas;
 SELECT MIN(id) AS 'MINIMO ID' , titulo FROM entradas;
+SELECT SUM(id) AS 'SUMA DE TODOS LOS ID' , titulo FROM entradas;
