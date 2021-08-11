7fca83c1efb8ced34bb7158822531d54916a3e50
gagan-daka <gagandeps0@gmail.com>
2021-08-11 18:47:24 +0200

    'inner join'

--- aprendiendo-sql/07-multitabla/consultas.sql
+++ aprendiendo-sql/07-multitabla/consultas.sql
@@ -3,9 +3,20 @@ CONSULTA MULTITABLA: consultas que sirven para consultar varias tablas en una so
 */
 
 /*MOSTRAR LAS ENTRADAS CON EL NOMBRE DEL AUTOR Y EL NOMBRE DE LA CATEGORIA*/
+    
+    /*CONSULTA SIN INNER JOIN*/
+    
 SELECT e.id, e.titulo, u.nombre AS 'Author', c.nombre AS 'Categoria'
 FROM entradas e, usuarios u, categorias c
 WHERE e.usuario_id = u.id AND e.categoria_id = c.id;
+    
+    /*CONSULTA CON INNER JOIN*/
+    
+/*----- INNER JOIN (MEZCLAME LA TABLA, HAZME UNA COMBINACIÓN INTERNA) -----*/
+SELECT e.id, e.titulo, u.nombre AS 'Author',  c.nombre AS 'Categoria'
+FROM entradas e
+INNER JOIN usuarios u ON e.usuario_id = u.id
+INNER JOIN categorias c ON e.usuario_id = c.id;
 
 /*MOSTRAR EL NOMBRE DE LAS CATEGORIAS Y AL LADO CUANTAS ENTRADAS TIENEN*/
 SELECT c.nombre, COUNT(e.id) FROM categorias c, entradas e
@@ -21,3 +32,5 @@ GROUP BY e.categoria_id;
 SELECT u.email, COUNT(titulo) FROM usuarios u, entradas e
 WHERE e.usuario_id = u.id
 GROUP BY e.usuario_id;
+
+
