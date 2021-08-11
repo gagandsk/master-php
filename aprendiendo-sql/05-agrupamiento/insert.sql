297e5110eccb0b000feb83c7a590c4ebd62cf89b
gagan-daka <gagandeps0@gmail.com>
2021-08-11 13:20:48 +0200

    inserts for table 'entradas'

--- aprendiendo-sql/05-agrupamiento/insert.sql
+++ aprendiendo-sql/05-agrupamiento/insert.sql
@@ -14,15 +14,15 @@ INSERT INTO categorias VALUES(null, 'Navegante');
 
 -- INSERTS  PARA ENTRADAS
     -- Usuario 1
-INSERT INTO entradas VALUES(null, 1, 32, 'Monkey D. Luffy', 'Llegar a ser el Rey de los Piratas', CURDATE());
-INSERT INTO entradas VALUES(null, 1, 33, 'Tony Tony Chopper', 'Ser capaz de curar cualquier enfermedad', CURDATE());
-INSERT INTO entradas VALUES(null, 1, 34, 'Sanji', 'Encontrar el "All Blue"', CURDATE());
+INSERT INTO entradas VALUES(null, 1, 1, 'Monkey D. Luffy', 'Llegar a ser el Rey de los Piratas', CURDATE());
+INSERT INTO entradas VALUES(null, 1, 2, 'Tony Tony Chopper', 'Ser capaz de curar cualquier enfermedad', CURDATE());
+INSERT INTO entradas VALUES(null, 1, 3, 'Sanji', 'Encontrar el "All Blue"', CURDATE());
     -- Usuario 2
-INSERT INTO entradas VALUES(null, 2, 36, 'Roronoa Zoro', 'Llegar a ser el mejor espadachín del mundo', CURDATE());
-INSERT INTO entradas VALUES(null, 2, 41, 'Nami', 'Dibujar un mapa de todo el mundo', CURDATE());
-INSERT INTO entradas VALUES(null, 2, 37, 'Usopp', 'Convertirse en un valiente pirata y visitar Elbaf, la tierra de los valientes guerreros', CURDATE());
+INSERT INTO entradas VALUES(null, 2, 5, 'Roronoa Zoro', 'Llegar a ser el mejor espadachín del mundo', CURDATE());
+INSERT INTO entradas VALUES(null, 2, 6, 'Usopp', 'Convertirse en un valiente pirata y visitar Elbaf, la tierra de los valientes guerreros', CURDATE());
+INSERT INTO entradas VALUES(null, 3, 7, 'Nico Robin', 'Encontrar el Rio Poneglyph y descubrir qué ocurrió en los cien años de historia perdida', CURDATE());
     -- Usuario 3
-INSERT INTO entradas VALUES(null, 3, 38, 'Nico Robin', 'Encontrar el Rio Poneglyph y descubrir qué ocurrió en los cien años de historia perdida', CURDATE());
-INSERT INTO entradas VALUES(null, 3, 39, 'Franky', 'Hacer un barco de ensueño y darle la vuelta al mundo como su carpintero de a bordo.', CURDATE());
-INSERT INTO entradas VALUES(null, 3, 40, 'Brook', '	Reencontrarse con Laboon', CURDATE());
+INSERT INTO entradas VALUES(null, 3, 8, 'Franky', 'Hacer un barco de ensueño y darle la vuelta al mundo como su carpintero de a bordo.', CURDATE());
+INSERT INTO entradas VALUES(null, 3, 9, 'Brook', '	Reencontrarse con Laboon', CURDATE());
+INSERT INTO entradas VALUES(null, 2, 10, 'Nami', 'Dibujar un mapa de todo el mundo', CURDATE());
 
