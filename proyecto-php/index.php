fa2bd2d942485d5edec3d0a8dbc9f28e6d2e9c68
gagan-daka <gagandeps0@gmail.com>
2021-08-12 22:39:00 +0200

    css - footer

--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -97,6 +97,18 @@
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
                 </a>
             </article>
+            <article class="entrada">
+                <a href="">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
+                </a>
+            </article>
+            <article class="entrada">
+                <a href="">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
+                </a>
+            </article>
             <div id="ver-todas"><a href="">Ver todas las entradas</a></div>
         </div>
         <div class="clearfix"></div>
