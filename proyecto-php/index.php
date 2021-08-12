c249a8e395af342c4b7a9b0062f03daf8dacfd63
gagan-daka <gagandeps0@gmail.com>
2021-08-12 14:21:19 +0200

    web layout - main content

--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog de One Piece</title>
 </head>
+
 <body>
     <!--CABECERA-->
-    <header id= "header">
+    <header id="cabecera">
         <!--LOGO-->
         <div id="logo">
             <a href="index.php">
@@ -17,7 +19,7 @@
         </div>
 
         <!--MENU-->
-        <nav id="nav">
+        <nav id="menu">
             <ul>
                 <li><a href="index.php">Inicio</a></li>
                 <li><a href="">Categoria 1</a></li>
@@ -31,9 +33,65 @@
 
     </header>
 
-    
-    <!--BARRA LATERAL-->
+    <div id="contenedor">
+        <!--BARRA LATERAL-->
+        <aside id="sidebar">
+            <div id="login" class="bloque">
+                <h3>Identificate</h3>
+                <form action="login.php" method="POST">
+                    <label for="email">Email</label>
+                    <input type="email" name="email" />
+
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password" />
+
+                    <input type="submit" value="Entrar">
+                </form>
+            </div>
+
+            <div id="register" class="bloque">
+                <h3>Registrate</h3>
+                <form action="registro.php" method="POST">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre" />
+
+                    <label for="apellidos">Apellidos</label>
+                    <input type="text" name="apellidos" />
+
+                    <label for="email">Email</label>
+                    <input type="email" name="email" />
+
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password" />
+
+                    <input type="submit" value="Registrar">
+                </form>
+            </div>
+        </aside>
+    </div>
+
     <!--CAJA PRINCIPAL-->
+    <div class="principal">
+        <h1>Ultimas entradas</h1>
+        <article class="entrada">
+            <h2>Titulo de mi entrada</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
+        </article>
+        <article class="entrada">
+            <h2>Titulo de mi entrada</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
+        </article>
+        <article class="entrada">
+            <h2>Titulo de mi entrada</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
+        </article>
+        <article class="entrada">
+            <h2>Titulo de mi entrada</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
+        </article>
+    </div>
+
     <!--PIE DE PÁGINA-->
 </body>
+
 </html>
