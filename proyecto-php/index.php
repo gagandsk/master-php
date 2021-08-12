b6e47d9cf88e32fbf8b22aa7a1e5f0646f152412
gagan-daka <gagandeps0@gmail.com>
2021-08-12 21:48:28 +0200

    css

--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -69,28 +69,39 @@
                 </form>
             </div>
         </aside>
+
+        <!--CAJA PRINCIPAL-->
+        <div id="principal">
+            <h1>Ultimas entradas</h1>
+            <article class="entrada">
+                <a href="">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
+                </a>
+            </article>
+            <article class="entrada">
+                <a href="">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
+                </a>
+            </article>
+            <article class="entrada">
+                <a href="">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
+                </a>
+            </article>
+            <article class="entrada">
+                <a href="">
+                    <h2>Titulo de mi entrada</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
+                </a>
+            </article>
+            <div id="ver-todas"><a href="">Ver todas las entradas</a></div>
+        </div>
+        <div class="clearfix"></div>
     </div>
 
-    <!--CAJA PRINCIPAL-->
-    <div class="principal">
-        <h1>Ultimas entradas</h1>
-        <article class="entrada">
-            <h2>Titulo de mi entrada</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
-        </article>
-        <article class="entrada">
-            <h2>Titulo de mi entrada</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
-        </article>
-        <article class="entrada">
-            <h2>Titulo de mi entrada</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
-        </article>
-        <article class="entrada">
-            <h2>Titulo de mi entrada</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cupiditate fugiat, quam eum sed blanditiis, reprehenderit magnam temporibus laborum pariatur, accusamus perferendis illo nulla! Blanditiis quae accusamus possimus. Fugiat, sint molestiae?</p>
-        </article>
-    </div>
 
     <!--PIE DE PÁGINA-->
     <footer id="pie">
