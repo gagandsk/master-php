e9b42ff91b812e2ca0f227d56b58b87b0492d94f
gagan-daka <gagandeps0@gmail.com>
2021-08-12 14:14:12 +0200

    web layout - menu

--- proyecto-php/index.php
+++ proyecto-php/index.php
@@ -8,14 +8,30 @@
 </head>
 <body>
     <!--CABECERA-->
-    <header>
+    <header id= "header">
+        <!--LOGO-->
         <div id="logo">
             <a href="index.php">
                 One Piece
             </a>
         </div>
+
+        <!--MENU-->
+        <nav id="nav">
+            <ul>
+                <li><a href="index.php">Inicio</a></li>
+                <li><a href="">Categoria 1</a></li>
+                <li><a href="">Categoria 2</a></li>
+                <li><a href="">Categoria 3</a></li>
+                <li><a href="">Categoria 4</a></li>
+                <li><a href="">Sobre mi</a></li>
+                <li><a href="">Contacto</a></li>
+            </ul>
+        </nav>
+
     </header>
-    <!--MENU-->
+
+    
     <!--BARRA LATERAL-->
     <!--CAJA PRINCIPAL-->
     <!--PIE DE PÁGINA-->
