1e976bc7470c71ba1d8ccbfc84ce36f46284860c
gagan-daka <gagandeps0@gmail.com>
2021-08-12 12:50:33 +0200

    exercise 28 sql

--- aprendiendo-sql/09-ejercicios-sql/ejercicio28.sql
+++ aprendiendo-sql/09-ejercicios-sql/ejercicio28.sql
@@ -3,6 +3,9 @@ MOSTRAR TODOS LOS VENDEDORES Y EL NUMERO DE CLIENTES.
 SE DEBEN MOSTRAR TENGAN O NO CLIENTES
 */
 
+
+
+
 SELECT v.nombre, v.apellidos, COUNT(c.id) FROM vendedores v
 LEFT JOIN clientes c ON c.vendedor_id = v.id
 GROUP BY v.id;
