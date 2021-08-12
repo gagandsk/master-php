8aa48912f907f8d6cd48e696788d1f3c3435fc02
gagan-daka <gagandeps0@gmail.com>
2021-08-12 14:11:14 +0200

    web layout - header

---
+++ proyecto-php/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog de One Piece</title>
+</head>
+<body>
+    <!--CABECERA-->
+    <header>
+        <div id="logo">
+            <a href="index.php">
+                One Piece
+            </a>
+        </div>
+    </header>
+    <!--MENU-->
+    <!--BARRA LATERAL-->
+    <!--CAJA PRINCIPAL-->
+    <!--PIE DE PÁGINA-->
+</body>
+</html>
