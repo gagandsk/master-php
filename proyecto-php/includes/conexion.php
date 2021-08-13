ad08c52b0fd908e65f33f6c2e5f3873aaeb9c577
gagan-daka <gagandeps0@gmail.com>
2021-08-13 12:36:11 +0200

    connection php

---
+++ proyecto-php/includes/conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+//CONEXION A LA BASE DE DATOS
+$server = 'localhost';
+$username = 'root';
+$password = '';
+$database = 'blog_master';
+
+$db = mysqli_connect($server, $username, $password, $database);
+
+mysqli_query($db, "SET NAMES 'utf8" );
+?>
