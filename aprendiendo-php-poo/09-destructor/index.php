55b3e1ea5326d754ed626fea3f8a512979216991
gagan-daka <gagandeps0@gmail.com>
2021-09-04 22:03:40 +0200

    OPP - __toString()

--- aprendiendo-php-poo/09-destructor/index.php
+++ aprendiendo-php-poo/09-destructor/index.php
@@ -5,17 +5,21 @@ class User{
     public $email;
 
     public function __construct(){
+        $this->nombre = "Gagan";
+        $this->email = "email@gmail.com";
         echo "Creando el objeto"."<br/>";
     }
 
     //A destructor is called when the object is destructed or the script is stopped or exited.
     public function __destruct(){
-        echo "Destruyendo el objeto";
+        echo "<br/>Destruyendo el objeto";
+    }
+
+    public function __toString()
+    {
+        return "Hola, {$this->nombre}, estas registrado con este email {$this->email}";
     }
 }
 
 $user = new User();
-
-for($i=0; $i<=200; $i++){
-    echo $i."<br/>";
-}
+echo $user; 
