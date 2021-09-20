5e5d5516c767a7a4881b44085f0b00875cbf5d37
gagan-daka <gagandeps0@gmail.com>
2021-09-20 19:33:38 +0200

    Design and creation of the database

--- proyecto-php-poo/database/database.sql
+++ proyecto-php-poo/database/database.sql
@@ -11,7 +11,58 @@ CREATE TABLE usuarios(
     password    varchar(255) not null,
     rol         varchar(20),
     imagen      varchar(255),
-    CONSTRAINT PK usuarios PRIMARY KEY (id),
-    CONSTRAINT uq_email UNIQUE(email) -- EL EMAIL SIEMPRE ES UN UNIQUE!!!!!!! SOLO PUEDE HABER 1 REGISTRO CON EL MISMO EMAIL
+    CONSTRAINT pk_usuarios PRIMARY KEY (id),
+    CONSTRAINT uq_email UNIQUE(email) -- SOLO PUEDE HABER 1 REGISTRO CON EL MISMO EMAIL
+)ENGINE=InnoDb; -- USAREMOS ESTE ENGINE PARA MANTENER LA INTEGRIDAD REFERENCIAL (LAS CLAVES AJENAS Y TODO ESO)
+-- aveces el on delete cascade es peligroso
+INSERT INTO usuarios VALUES(null, 'Admin','Admin','admin@admin.com','admin','admin', null);
 
-)ENGINE=InnoDb; --USAREMOS ESTE ENGINE PARA MANTENER LA INTEGRIDAD REFERENCIAL (LAS CLAVES AJENAS Y TODO ESO)
+CREATE TABLE categorias(
+    id          int(255) AUTO_INCREMENT not null,
+    nombre      varchar(100) not null,
+    CONSTRAINT pk_categorias PRIMARY KEY (id)
+)ENGINE=InnoDb;
+
+INSERT INTO categorias VALUES (null, 'Manga Corta');
+INSERT INTO categorias VALUES (null, 'Tirantes');
+INSERT INTO categorias VALUES (null, 'Manga Larga');
+INSERT INTO categorias VALUES (null, 'Sudaderas');
+
+
+CREATE TABLE productos(
+    id              int(255) AUTO_INCREMENT not null,
+    categoria_id    int(255) not null,
+    nombre          varchar(100) not null,
+    descripcion     text,
+    precio          float(100,2)not null,
+    stock           int (255) not null,
+    oferta          varchar(2),
+    fecha           date not null,
+    imagen          varchar(255),
+    CONSTRAINT      pk_categorias PRIMARY KEY (id),
+    CONSTRAINT      fk_producto_categoria FOREIGN KEY (categoria_id) REFERENCES categorias(id)
+)ENGINE=InnoDb; 
+
+
+CREATE TABLE pedidos(
+    id              int(255) AUTO_INCREMENT not null,
+    usuario_id      int(255) not null,
+    provincia       varchar(100) not null,
+    localidad       varchar(100) not null,
+    direccion       varchar(255) not null, 
+    coste           float(200,2) not null,
+    estado          varchar(20) not null,
+    fecha           date,
+    hora            time,
+    CONSTRAINT      pk_pedidos PRIMARY KEY (id),
+    CONSTRAINT      fk_pedidos_usuario FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
+)ENGINE=InnoDb; 
+
+CREATE TABLE lineas_pedidos(
+    id              int(255) AUTO_INCREMENT not null,
+    pedido_id       int(255) not null,
+    producto_id     int(255) not null,
+    CONSTRAINT      pk_lineas_pedidos PRIMARY KEY (id),
+    CONSTRAINT      fk_lineas_pedidos FOREIGN KEY (pedido_id) REFERENCES pedidos(id),
+    CONSTRAINT      fk_lineas_producto FOREIGN KEY (producto_id) REFERENCES productos(id)
+)ENGINE=InnoDb; 
